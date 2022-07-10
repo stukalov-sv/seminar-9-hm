b09513110f104a7eb0eb3da91b4c8226bad205f5
Sergei <energostuk@gmail.com>
2022-07-10 21:53:03 +0300

    Add tasks

---
+++ Task66HM/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдет
+сумму натуральных элементов в промежутке от M и N
+*/
+
+int GetSumNumbers(int numberN, int numberM, int sum)
+{
+    if (numberN >= numberM)
+    {
+        int count = numberM;
+        sum = sum + count;
+        numberM++;
+        return GetSumNumbers(numberN, numberM, sum);
+    }
+    else
+        return (sum);
+}
+
+Console.WriteLine("Enter natural number M: ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter natural number N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+if (numberN > numberM)
+{
+    int result = GetSumNumbers(numberN, numberM, sum);
+    Console.WriteLine("Amount of natural numbers: ");
+    Console.WriteLine(result);
+}
+else
+    Console.WriteLine("Incorrect data. N must be bigger than M");
---
+++ Task68HM/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n
+*/
+
+int AckermannFunction(int numberM, int numberN)
+{
+    if (numberM == 0)
+        return numberN + 1;
+    else if (numberN == 0)
+        return AckermannFunction(numberM, 1);
+    else
+        return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+}
+
+Console.WriteLine("Enter positive number M: ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter positive number N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int result = AckermannFunction(numberM, numberN);
+Console.WriteLine($"A(m,n) = {result}");
